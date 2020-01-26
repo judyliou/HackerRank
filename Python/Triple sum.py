74479f51737a6242c42416d355a63996596b7503
Judy Liou <37167429+judyliou@users.noreply.github.com>
2020-01-26 14:48:55 -0500

    Create Triple sum.py

---
+++ Python/Triple sum.py
@@ -0,0 +1,16 @@
+def triplets(a, b, c):
+    a = sorted(list(set(a)))
+    b = sorted(list(set(b)))
+    c = sorted(list(set(c)))
+    
+    p1, p3 = 0, 0
+    cnt = 0
+    for i in range(len(b)):
+        while p1 < len(a):
+            if a[p1] > b[i]: break    
+            p1 += 1
+        while p3 < len(c):
+            if c[p3] > b[i]: break
+            p3 += 1  
+        cnt += p1 * p3        
+    return cnt
